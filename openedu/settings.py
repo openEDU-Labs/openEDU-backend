559b0717eb3510f5921252181326c1123ecf6bc0
Mark Schmidt <schmidtwmark@gmail.com>
2015-10-19 15:32:19 -0500

    undo that

--- openedu/settings.py
+++ openedu/settings.py
@@ -72,6 +72,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'openedu.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
